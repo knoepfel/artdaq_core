3b0c2988d1a08551c1a29a2adca82b783606a243
John Freeman <jcfree@fnal.gov>
2014-09-14 10:54:04 -0500

    New nomenclature: the struct which returns this package's PackageBuildInfo struct is now called GetPackageBuildInfo; this is also the name of the files which define it

--- artdaq-core/ArtModules/ArtdaqCoreBuildInfo_module.cc
+++ artdaq-core/ArtModules/ArtdaqCoreBuildInfo_module.cc
@@ -1,13 +1,13 @@
-#include "artdaq-core/BuildInfo/GetPackageInfo.hh"
-
 #include "artdaq-core/ArtModules/BuildInfo_module.hh"
+
+#include "artdaq-core/BuildInfo/GetPackageBuildInfo.hh"
 
 #include <string>
 
 namespace artdaq {
 
   static std::string instanceName = "ArtdaqCoreBuildInfo";
-  typedef artdaq::BuildInfo< &instanceName, artdaqcore::PackageInfo> ArtdaqCoreBuildInfo;
+  typedef artdaq::BuildInfo< &instanceName, artdaqcore::GetPackageBuildInfo> ArtdaqCoreBuildInfo;
 
   DEFINE_ART_MODULE(ArtdaqCoreBuildInfo)
 }
