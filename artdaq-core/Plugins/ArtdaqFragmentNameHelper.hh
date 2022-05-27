2ae1c03766bd26099ce2dbcc0e592d1ced83e637
Eric Flumerfelt <eflumerf@fnal.gov>
2022-05-27 08:34:26 -0500

    Default implementation in FragmentNameHelper base class (so that others like ArtdaqDemoFragmentNameHelper don't have to link against the plugin)

--- artdaq-core/Plugins/ArtdaqFragmentNameHelper.hh
+++ artdaq-core/Plugins/ArtdaqFragmentNameHelper.hh
@@ -27,28 +27,6 @@ namespace artdaq {
 		*/
 		ArtdaqFragmentNameHelper(std::string unidentified_instance_name, std::vector<std::pair<artdaq::Fragment::type_t, std::string>> extraTypes);
 
-		/**
-				 * \brief Returns the basic translation for the specified type.  Defaults to the specified
-				 *        unidentified_instance_name if no translation can be found.
-				 */
-		virtual std::string GetInstanceNameForType(artdaq::Fragment::type_t type_id) const;
-
-		/**
-				 * \brief Returns the full set of product instance names which may be present in the data, based on
-				 *        the types that have been specified in the SetBasicTypes() and AddExtraType() methods.  This
-				 *        *does* include "container" types, if the container type mapping is part of the basic types.
-				 */
-		virtual std::set<std::string> GetAllProductInstanceNames() const;
-
-		/**
-				 * \brief Returns the product instance name for the specified fragment, based on the types that have
-				 *        been specified in the SetBasicTypes() and AddExtraType() methods.  This *does* include the
-				 *        use of "container" types, if the container type mapping is part of the basic types.  If no
-				 *        mapping is found, the specified unidentified_instance_name is returned.
-				 */
-		virtual std::pair<bool, std::string>
-			GetInstanceNameForFragment(artdaq::Fragment const& fragment) const;
-
 	private:
 		ArtdaqFragmentNameHelper(ArtdaqFragmentNameHelper const&) = delete;
 		ArtdaqFragmentNameHelper(ArtdaqFragmentNameHelper&&) = delete;
